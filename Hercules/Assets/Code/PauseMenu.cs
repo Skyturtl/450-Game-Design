663f84ddc25a4bd734d0e6545e11fb34c0ecd261
guomeganz <guo.m.z@wustl.edu>
2024-09-30 15:31:25 -0500

    Prefabs Resolved

--- Hercules/Assets/Code/PauseMenu.cs
+++ Hercules/Assets/Code/PauseMenu.cs
@@ -33,7 +33,7 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
-        isPaused = true
+        isPaused = true;
     }
 
     public void ResumeGame()
