d051d7ab0f2ef04818a93be137ea9c2c2d1d8a33
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2024-10-04 16:53:02 -0500

    Revert "Resolving Collision"
    This reverts commit b5e2f2de77d9ce37b0b70e0b715ce277b7f76fe2.

--- Hercules/Assets/Code/Projectile.cs
+++ Hercules/Assets/Code/Projectile.cs
@@ -4,34 +4,23 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    public float damageAmount = 5f;
     //Outlets
     Rigidbody2D _rigidbody2D;
-    public float damageAmount = 5f;
 
+    // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _rigidbody2D.velocity = transform.right * 20f;
     }
     
-<<<<<<< Updated upstream
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
         if(collision.gameObject.CompareTag("Enemy")){
             collision.gameObject.GetComponent<Enemy>().TakeDamage(damageAmount);
-=======
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Debug.Log("Hit");
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            other.gameObject.GetComponent<Enemy>().TakeDamage(damageAmount);
-            if (other.gameObject.GetComponent<Enemy>().health <= 0)
-            {
-                Destroy(other.gameObject);
-            }
->>>>>>> Stashed changes
             Destroy(gameObject);
         }
     }
